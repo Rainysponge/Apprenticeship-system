ccce828101b2804ac900adb6ab1b2dbef2638588
Rainysponge <375364412@qq.com>
2020-10-25 10:27:04 +0800

    update settings

--- ApprenticeshipSystem/ApprenticeshipSystem/settings.py
+++ ApprenticeshipSystem/ApprenticeshipSystem/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pymysql import install_as_MySQLdb
+
+install_as_MySQLdb()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'UTC'
 
