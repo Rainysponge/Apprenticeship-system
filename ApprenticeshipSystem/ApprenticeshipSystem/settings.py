ff4047663d78f0c8b0404e64373a0db4306a3f10
Rainysponge <375364412@qq.com>
2020-10-25 10:57:56 +0800

    update login and register (having not done)

--- ApprenticeshipSystem/ApprenticeshipSystem/settings.py
+++ ApprenticeshipSystem/ApprenticeshipSystem/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Apprenticeship',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,9 @@ ROOT_URLCONF = 'ApprenticeshipSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- ApprenticeshipSystem/ApprenticeshipSystem/urls.py
+++ ApprenticeshipSystem/ApprenticeshipSystem/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
