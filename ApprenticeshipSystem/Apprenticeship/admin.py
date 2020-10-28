a10f1b040b16986ceb0f69cfe2058792b54e2e06
Delicate2000 <1772354795@qq.com>
2020-10-28 14:20:33 +0800

    updates models

--- ApprenticeshipSystem/Apprenticeship/admin.py
+++ ApprenticeshipSystem/Apprenticeship/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+
+@admin.register(Student)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'student_name', 'student_sex', 'school', 'enter_time', 'grade', 'help', 'self_introduction')
+
+
+@admin.register(Teacher)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'teacher_name', 'teacher_sex', 'school', 'enter_time', 'grade', 'skill', 'self_introduction')
+
+
+@admin.register(Relationship)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'SID', 'TID', 'created_time')
+
+
+@admin.register(Homework)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'relationship_id', 'content')
+
+
+@admin.register(Critic)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'homework_id', 'content')
+
+
+@admin.register(Major)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'major')
+
+
+@admin.register(Sex)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'sex')
--- ApprenticeshipSystem/Apprenticeship/models.py
+++ ApprenticeshipSystem/Apprenticeship/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor_uploader.fields import RichTextUploadingField
+
 
 # Create your models here.
 
@@ -13,3 +15,70 @@ from django.contrib.auth.models import User
 # 也赋予了user既是学生有时老师的可能  {% if user.is_student and user.is_teacher %}
 # 但实现上还需要斟酌，或者对于三种用户分别编写页面
 # 也可以使用AbstractUser(个人跟倾向于这个)
+
+
+class Major(models.Model):
+    major = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.major
+
+
+class Sex(models.Model):
+    sex = models.CharField(max_length=1)  # 怎么限制男女？？？
+
+    def __str__(self):
+        return self.sex
+
+
+class Teacher(models.Model):
+    teacher_name = models.CharField(max_length=10)
+    teacher_sex = models.ForeignKey(Sex, on_delete=models.DO_NOTHING)
+    school = models.CharField(max_length=10)
+    enter_time = models.DateTimeField()
+    grade = models.CharField(max_length=5)
+    major = models.ForeignKey(Major, on_delete=models.DO_NOTHING)
+    skill = models.CharField(max_length=50)
+    self_introduction = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.teacher_name
+
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=10)
+    student_sex = models.ForeignKey(Sex, on_delete=models.DO_NOTHING)
+    school = models.CharField(max_length=10)
+    enter_time = models.DateTimeField()
+    grade = models.CharField(max_length=5)
+    major = models.ForeignKey(Major, on_delete=models.DO_NOTHING, null=True)  # 为什么就这里会报错？？？
+    help = models.CharField(max_length=50)
+    self_introduction = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.student_name
+
+
+class Relationship(models.Model):
+    SID = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    TID = models.ForeignKey(Teacher, on_delete=models.DO_NOTHING)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
+
+
+class Homework(models.Model):
+    relationship_id = models.ForeignKey(Relationship, on_delete=models.DO_NOTHING)
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.content
+
+
+class Critic(models.Model):
+    homework_id = models.ForeignKey(Homework, on_delete=models.DO_NOTHING)
+    content = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.content
